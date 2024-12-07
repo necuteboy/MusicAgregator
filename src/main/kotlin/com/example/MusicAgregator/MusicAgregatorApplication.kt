bb20ef88e7942837e1096e418d25399cff894ff6
necuteboy <andreychikov32@mail.ru>
2024-12-08 02:17:01 +0500

    init commit

---
+++ src/main/kotlin/com/example/MusicAgregator/MusicAgregatorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.MusicAgregator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusicAgregatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<MusicAgregatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/MusicAgregator/MusicAgregatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.MusicAgregator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusicAgregatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
